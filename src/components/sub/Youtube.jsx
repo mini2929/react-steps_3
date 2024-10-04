172ba6b5720902c2fd3ed2bd04faaf9218d7fe6a
mini2929 <haloyun29@gmail.com>
2024-10-04 09:51:51 +0900

    Youtube-useState 사용 기초

--- src/components/sub/Youtube.jsx
+++ src/components/sub/Youtube.jsx
@@ -1,9 +1,16 @@
+import {useState} from 'react';
 import Layout from '../common/Layout';
 
 export default function Youtube() {
+	const [Num, setNum] = useState(0);
+	const minusNum = () => {
+		setNum(Num-1);
+	};
 	return (
 		<Layout title={'YOUTUBE'}>
-			<p>Youtube Page contents come here.</p>
+			<h2>{Num}</h2>
+			<button onClick={minusNum}>minus</button>
+			<button onClick={() => setNum(Num+1)}>plus</button>
 		</Layout>
 	);
 }
